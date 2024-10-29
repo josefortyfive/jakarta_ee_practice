77132c70d09d9ba2c947645b81df3e706cab5e3d
josefortyfive <jose.fortyfive@gmail.com>
2024-10-29 16:57:05 +0800

    Added StudenControllerServlet.java

---
+++ web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -0,0 +1,68 @@
+package com.luv2code.web.jdbc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet("/StudentControllerServlet")
+public class StudentControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private StudentDbUtil studentDbUtil;
+
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		// create our student db util ... and pass in the conn pool / datasource
+		
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+		}
+		catch(Exception exc) {
+			throw new ServletException(exc);
+		}
+		
+	}
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// list the students ... in MVC fashion
+		
+		try {
+			listStudents(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+
+	private void listStudents(HttpServletRequest request, HttpServletResponse response)  throws Exception {
+		
+		List<Student> students = studentDbUtil.getStudents();
+		
+		
+		request.setAttribute("STUDENT_LIST", students);
+		
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-student.jsp");
+		dispatcher.forward(request, response);
+	
+	}
+
+}
