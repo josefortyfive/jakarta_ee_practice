3737a44ee551bde6b4b11ab249424c017ae77b2f
josefortyfive <jose.fortyfive@gmail.com>
2024-11-06 11:42:24 +0800

    Created function for update student details

--- web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -58,7 +58,9 @@ public class StudentControllerServlet extends HttpServlet {
 				case "ADD":
 					addStudent(request, response);
 					break;
-					
+				case "LOAD":
+					loadStudent(request, response);
+					break;
 				default:
 					listStudents(request, response);
 			}
@@ -71,6 +73,22 @@ public class StudentControllerServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 
+	}
+
+	private void loadStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// read student id from data
+		String theStudentId = request.getParameter("studentId");
+		
+		// get student from database (db util)
+		Student theStudent = studentDbUtil.getStudents(theStudentId);
+		
+		// place student in the request attribute
+		request.setAttribute("THE_STUDENT", theStudent);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/update-student-form.jsp");
+		
+		dispatcher.forward(request, response);
+		
 	}
 
 	private void addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
